a149983c03040301a79315eba6a8c112d3ee5ba6
Jonathan Chan <jc@jmccc.com>
2024-07-05 23:34:38 -0500

    hacking the ability to display icons from the internet

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	toast "gopkg.in/toast.v1"
+
+	"net/http"
+	"os"
 )
 
 // Overridden via ldflags
@@ -36,6 +41,21 @@ func main() {
 				_ = cmd.Usage()
 				return
 			}
+
+			if len(icon) > 0 && (icon[:7] == "http://" || icon[:8] == "https://") {
+				tmpFolder := os.TempDir()
+
+				err := DownloadFile(icon, filepath.Join(tmpFolder, "wsl-notify-send-icon-tmp.png"))
+				if err != nil {
+					log.Fatalln(err)
+					icon = ""
+				} else {
+					// had to comment this out because the toast wasn't getting invoked before the file was removed
+					// defer os.Remove("wsl-notify-send-icon-tmp.png")
+					icon = filepath.Join(tmpFolder, "wsl-notify-send-icon-tmp.png")
+				}
+			}
+
 			notification := &toast.Notification{
 				AppID:   appID,
 				Title:   category,
@@ -60,6 +80,33 @@ func main() {
 	rootCmd.Flags().StringVar(&appID, "appId", "wsl-notify-send", "[non-standard] Specifies the app ID")
 	rootCmd.Flags().BoolVar(&showVersion, "version", false, "Show version information")
 	_ = rootCmd.Execute()
+}
+
+// DownloadFile will download a url and store it in local filepath.
+// It writes to the destination file as it downloads it, without
+// loading the entire file into memory.
+func DownloadFile(url string, filepath string) error {
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // TODO - explore mapping icons: https://wiki.ubuntu.com/NotificationDevelopmentGuidelines#How_do_I_get_these_slick_icons
